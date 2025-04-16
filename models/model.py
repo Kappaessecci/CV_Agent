b9a970012706cdc8003a7d763c94a70a1d3f1614
GiorgiaVitanza <vitanzagiorgia2@gmail.com>
2025-04-16 12:59:56 +0200

    modifica prompt e gestione eccezioni nel template di output

--- models/model.py
+++ models/model.py
@@ -27,7 +27,7 @@ def optimize_cv(uploaded_cv, job_description, api_key):
     """
     Function to optimize a CV based on a job description.
     """
-    optimized_cv = api_query(api_key, f"Optimize the following sections Interests and About me for the job description: {job_description}. Use 40 words. CV: {uploaded_cv}")
+    optimized_cv = api_query(api_key, f"Optimize the following CV {uploaded_cv} for the job description: {job_description} in the Interests and About me sections.")
     
 
     return optimized_cv 
--- utils/templates.py
+++ utils/templates.py
@@ -4,19 +4,35 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 
 def generate_txt(result):
-    # Estrai il testo dal dizionario (adatta alla tua struttura)
-    text = result["candidates"][0]["content"]["parts"][0]["text"]
+    try:
+        contents = result.get("contents", [])
+        if not contents:
+            return "⚠️ Nessun contenuto restituito dall'API."
 
-    buffer = io.BytesIO()  # crea un buffer in memoria
-    c = canvas.Canvas(buffer, pagesize=A4)
-    c.setFont("Helvetica", 10)
+        parts = contents[0].get("parts", [])
+        if not parts:
+            return "⚠️ Nessuna parte trovata nei contenuti."
 
-    y = 800
-    for line in text.split("\n"):
-        c.drawString(50, y, line)
-        y -= 15
+        text = parts[0].get("text", "")
+        if not text:
+            return "⚠️ Testo vuoto."
+        buffer = io.BytesIO()  # crea un buffer in memoria
+        c = canvas.Canvas(buffer, pagesize=A4)
+        c.setFont("Helvetica", 10)
 
-    c.save()
+        y = 800
+        for line in text:
+            c.drawString(50, y, line)
+            y -= 15
 
-    buffer.seek(0)  # torna all'inizio del buffer
-    return buffer.read()  # restituisce il contenuto binario del PDF
+        c.save()
+
+        buffer.seek(0)  # torna all'inizio del buffer
+        return buffer.read()  # restituisce il contenuto binario del PDF
+
+    
+
+    except Exception as e:
+        return f"❌ Errore imprevisto: {e}"
+
+   
